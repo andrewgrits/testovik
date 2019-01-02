416bc58462f97041c9bbe289142bafa7195b423d
Andrew Mozart <andrew_gri@mail.ru>
2019-01-02 20:22:17 +0200

    add project

---
+++ Data/Context/BaseDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Context
+{
+    public class BaseDbContext : DbContext
+    {
+        public BaseDbContext(DbContextOptions<BaseDbContext> options) : base(options)
+        { }
+
+        public DbSet<TestEntity> TestEntities { get; set; }
+    }
+}
---
+++ Data/Entities/TestEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Entities
+{
+    public class TestEntity
+    {
+        public Guid Id { get; set; }
+        public string DataString { get; set; }
+    }
+}
